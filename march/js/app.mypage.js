266b113af9a042c9c47ac3ba0fbadbe766b2f24e
africalib <africalibrary21@gmail.com>
2020-12-04 02:26:37 +0900

    editted

--- march/js/app.mypage.js
+++ march/js/app.mypage.js
@@ -4,16 +4,28 @@ let app = new Vue({
     el: '#app',
     data: {
         user: {
-            wins: 0,
-            losses: 0,
-            firstGameDate: '(none)',
-            lastGameDate: '(none)'
+            data: {
+                wins: 0,
+                losses: 0,
+                firstGameDate: '(none)',
+                lastGameDate: '(none)'
+            },
+            default: {}
+        }
+    },
+    methods: {
+        reset: function () {
+            if (confirm('기록을 초기화하시겠습니까?')) {
+                localStorage.removeItem('user');
+                this.user.data = appLib.renew(this.user.default);
+            }
         }
     },
     created: function () {
         let user = localStorage.getItem('user');
+        this.user.default = appLib.renew(this.user.data);
 
         if (user)
-            this.user = JSON.parse(user);
+            this.user.data = JSON.parse(user);
     }
 });
