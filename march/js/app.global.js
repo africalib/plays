8c1ecb08d88a9c104722139344047faae6bf0a9c
africalib <africalibrary21@gmail.com>
2021-09-10 19:45:46 +0900

    updated

--- march/js/app.global.js
+++ march/js/app.global.js
@@ -1,4 +1,5 @@
 const global = {
-    baseUrl: 'http://172.30.1.23:8000'
-    // baseUrl: 'https://africalib.gabia.io'
+    // baseUrl: 'http://localhost:8000'
+    // baseUrl: 'http://172.30.1.23:8000'
+    baseUrl: 'https://africalib.gabia.io'
 };
